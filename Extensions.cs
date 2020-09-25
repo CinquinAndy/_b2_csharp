c8065d6129119c5d44a61389add836e139bb3ca2
Andy <cinquin.andy@gmail.com>
2020-09-25 11:59:41 +0200

    Fin de cours

---
+++ Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2020
+{
+    public static class Extensions
+    {
+        public static void affichageInfo(this string affichage){
+            Console.WriteLine(affichage);
+        }
+    }
+}
--- Personnage.cs
+++ Personnage.cs
@@ -20,12 +20,10 @@ namespace _2020
         public Personnage(string nomPerso, Metier metierPerso, List<Arme> listeArmes,
             int viePerso = 25, int vitessePerso=25, int forcePerso=25, int endurancePerso=25)
         {
-            
             this.NomPerso= nomPerso;
             this.MetierPerso= metierPerso;
             this.ViePerso = viePerso;
             this.ListeArmes = listeArmes;
-
             // possibilité 1
             if (endurancePerso + vitessePerso + forcePerso >= 25)
             {
@@ -39,17 +37,22 @@ namespace _2020
                 this.ForcePerso= forcePerso;
             }
             // possibilité 2 -> retour erreur
-
             // possibilité 3 -> gestion de la saisie, avec un prompt
-
             // Console.ReadLine(); + do while pour gestion de saisie tant que saisie pas ok
-
             //this.armesPerso =new Arme[this.forcePerso];
         }
-
         public string NomPerso { get => nomPerso; set => nomPerso = value; }
         public int VitessePerso { get => vitessePerso; set => vitessePerso = value; }
-        public int ForcePerso { get => forcePerso; set => forcePerso = value; }
+        public int ForcePerso { get => forcePerso; set { 
+                if (vitessePerso + endurancePerso + value >= 25){}
+                else
+                {
+                    forcePerso = value;
+                    // pour accédé à mon extension, il faut que la variable soit une string
+                    forcePerso.ToString().affichageInfo();
+                }
+            } 
+        }
         public int EndurancePerso { get => endurancePerso; set => endurancePerso = value; }
         public int ViePerso { get => viePerso; set => viePerso = value; }
         internal Metier MetierPerso { get => metierPerso; set => metierPerso = value; }
